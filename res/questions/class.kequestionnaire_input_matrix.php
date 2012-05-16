cb328611e632debdf6e626c751aee9a493ac9d69
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-16 06:17:57 +0000

    removed debug entries
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_questionnaire/trunk@62346 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/questions/class.kequestionnaire_input_matrix.php
+++ res/questions/class.kequestionnaire_input_matrix.php
@@ -348,7 +348,6 @@ class kequestionnaire_input_matrix extends kequestionnaire_input{
                 
                 $tmplCol=$this->cObj->getSubpart($this->tmpl, '###SLIDER###');
                 $header = $this->cObj->getSubpart($this->cObj->fileResource($this->template), '###HEADER_ADDITIONS###');
-                t3lib_utility_Debug::debug($question, 'question');
                 $GLOBALS['TSFE']->register['kequestionnaire'][$question['question_uid']] .= '
                         $( "#keq_'.$question['question_uid'].'_'.$question['uid'].'" ).val(1);
                         $( "#span_'.$question['question_uid'].'_'.$question['uid'].'" ).text(id2title(1));
